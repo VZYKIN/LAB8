6e08ed3ed7c8447e6fbfff47dc928e76720aac42
VZYKIN <92172085+VZYKIN@users.noreply.github.com>
2021-11-18 04:53:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
+def  kbig(item):
+   return item[1]
 
-import random
-def kbig(nums, k)
-nums = [random.randint(1, 3) for i in range(5)]
-
-print('Сгенерированы такие числа: ')
-print(nums)
-
-sorted_nums = sorted(nums, reverse=True)
-print('В порядке убывания: ')
-print(sorted_nums)
-sorted_nums = sorted(set(sorted_nums), reverse=True)
-
-for i, num in enumerate(sorted_nums, 1):
-    print('{i} максимум: {maxim}'.format(i=i, maxim=num))
+unordered = [('b', 'b'), ('c', 'd'), ('d', 'a'), ('a', 'c')]
+unordered.sort(key=keyFunc, reverse = True)
+print('Ordered list:', unordered)
